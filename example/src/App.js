c60aae8a7b6b08adb716a375dacd122bb725fa91
Julian Dong <huydt@dgroup.co>
2020-04-26 21:08:04 +0700

    init commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-import { ExampleComponent } from 'my-fancy-button'
+import { MyFancyButton } from 'my-fancy-button'
 import 'my-fancy-button/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div style={{ justifyContent: 'space-between', padding: 10, display: 'flex'}}>
+      <MyFancyButton onClick={() => console.log('Default')} text="Default" />
+      <MyFancyButton onClick={() => console.log('Primary')} text="Primary" type="primary" />
+      <MyFancyButton onClick={() => console.log('Danger')} text="Danger" type="danger" />
+      <MyFancyButton onClick={() => console.log('Disabled')} text="Disabled" type="disabled" disabled />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from "react";
+import styles from "./styles.module.css";
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export const MyFancyButton = ({
+  text,
+  type = "default",
+  disabled,
+  onClick
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`${styles.button} ${styles[type]} `}
+    >
+      {text}
+    </button>
+  );
+};
